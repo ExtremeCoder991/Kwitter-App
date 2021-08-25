c45669bd79fd9895d1eb97e373056ba863216f93
ExtremeCoder991 <75404903+ExtremeCoder991@users.noreply.github.com>
2021-08-24 17:19:51 -0700

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCfhZoex4ACGAQUyPZomMVuir6kfhKZ28k",
+    authDomain: "kwitter-project-a4e1c.firebaseapp.com",
+    databaseURL: "https://kwitter-project-a4e1c-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-a4e1c",
+    storageBucket: "kwitter-project-a4e1c.appspot.com",
+    messagingSenderId: "972686672181",
+    appId: "1:972686672181:web:91c70268de2b885c994c7c",
+    measurementId: "G-FTJ3L2RN83"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
