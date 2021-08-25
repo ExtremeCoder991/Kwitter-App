aaee3da49fd0cad26ad903c322b40d3003a265f6
ExtremeCoder991 <75404903+ExtremeCoder991@users.noreply.github.com>
2021-08-24 19:18:41 -0700

    Add files via upload

--- Kwitter_room.js
+++ Kwitter_room.js
@@ -11,3 +11,40 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+
+
+function addRoom() {
+    room_name = document.getElementById("room_name").value;
+
+    firebase.database().ref("/").child(room_name).update({
+        purpose: "adding room name"
+    });
+
+    localStorage.setItem("room_name", room_name);
+
+    window.location = "Kwitter_page.html";
+}
+
+function getData() {
+    firebase.database().ref("/").on('value',
+        function (snapshot) {
+            document.getElementById("output").innerHTML =
+                "";
+            snapshot.forEach(function (childSnapshot) {
+                childKey = childSnapshot.key;
+                Room_names = childKey;
+                //Start code
+                console.log("Room Name - " + Room_names);
+                row = "<div class='room_name' id=" + Room_names + "onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+                document.getElementById("output").innerHTML += row;
+                //End code
+            });
+        });
+}
+getData();
+
+function redirectToRoomName(name) {
+    console.log(name);
+    localStorage.setItem("room_name", name);
+    window.location = "kwitter_page.html";
+}
